5bad3739beb91ada9ca549466b87a22cdd7ca3a0
Kristóf <kristef.g@gmail.com>
2022-02-16 20:16:46 +0100

    starter setup with express server and react client typescript template

---
+++ client/src/App.tsx
@@ -0,0 +1,13 @@
+const App = () => {
+  return (
+    <div className='App'>
+      <h1> Hello there</h1>
+      <p>Update code</p>
+      <p>Update code</p>
+      <p>Update code</p>
+      <p>Update code</p>
+    </div>
+  );
+};
+
+export default App;
